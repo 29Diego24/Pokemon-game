427c5ff71b4ea09099471d81ee18735b87699482
David Penn <dpenn826@insite.4cd.edu>
2025-02-10 15:40:05 -0800

    22

--- trainerBattle.py
+++ trainerBattle.py
@@ -5,7 +5,7 @@ class Trainer:
     def __init__(self, pokemonList, myPokemon, other, heals, statusheals, heal):
         self._pokemonList = pokemonList
         self._pokemon = self._pokemonList[0]
-        self.best_pokemon()
+        self.worst_pokemon()
         self._other = other
         self._heals = heals
         self._statusheals = statusheals
@@ -18,9 +18,22 @@ class Trainer:
         """Set the opposing Pokémon."""
         self._other = pokemon
 
+    def switch_worst_pokemon(self):
+        worst = 0
+        for pokemon in self._pokemonList:
+            if not pokemon.isFainted():
+                attackDamage = max(pokemon._attacks.values())
+                if attackDamage < worst:
+                    if pokemon != self._pokemon:
+                        worst = attackDamage
+                        index = self._pokemonList.index(pokemon)
+
+        self._pokemon = self._pokemonList[index]
+        return True
+
     def switch_to_valid_pokemon(self):
         """Switch to a valid (non-fainted) Pokémon if possible."""
-        if self.best_pokemon():
+        if self.switch_worst_pokemon():
             print(f"The trainer switched to {self._pokemon._name}")
             self._faintedPokemon = None
             return True
@@ -64,14 +77,37 @@ class Trainer:
                     return True
 
         # Heal status conditions if needed.
-        if any([self._pokemon._paralyzed, self._pokemon._poisoned, self._pokemon._burn, self._pokemon._freeze, self._pokemon._sleep]):
+        hasStatusHeal = False
+        if any([self._pokemon._paralyzed, self._pokemon._poisoned, self._pokemon._burn, self._pokemon._freeze, self._pokemon._sleep, self._pokemon._confuse]):
             for heal_item in self._heals:
                 if isinstance(heal_item, str) and (heal_item in list(self._statusheals.keys())):
-                    self._statusheals[heal_item](self._pokemon)
-                    print(f"The trainer used a(n) {heal_item}")
-                    print(f"{self._pokemon._name}'s status condition was healed.")
-                    self._heals.remove(heal_item)
-                    return True
+                    if self._pokemon._paralyzed:
+                        if heal_item == "paralyze heal":
+                            hasStatusHeal = True
+                    elif self._pokemon._poisoned:
+                        if heal_item == "antidote":
+                            hasStatusHeal = True
+                    elif self._pokemon._burn:
+                        if heal_item == "burn heal":
+                            hasStatusHeal = True
+                    elif self._pokemon._freeze:
+                        if heal_item == "ice heal":
+                            hasStatusHeal = True
+                    elif self._pokemon._sleep:
+                        if heal_item == "awakening":
+                            hasStatusHeal = True
+                    elif self._pokemon._confuse:
+                        if heal_item == "bitter berry":
+                            hasStatusHeal = True
+                    elif heal_item in ["full heal", "full restore"]:
+                        hasStatusHeal = True
+
+                    if hasStatusHeal:
+                        self._statusheals[heal_item](self._pokemon)
+                        print(f"The trainer used a(n) {heal_item}")
+                        print(f"{self._pokemon._name}'s status condition was healed.")
+                        self._heals.remove(heal_item)
+                        return True
 
         # Revive a fainted Pokémon if possible.
         for pokemon in self._pokemonList:
@@ -92,13 +128,13 @@ class Trainer:
             if not available_pokemon:
                 return True
             
-    def best_pokemon(self):
-        best = 0
+    def worst_pokemon(self):
+        worst = 0
         for pokemon in self._pokemonList:
             if not pokemon.isFainted():
                 attackDamage = max(pokemon._attacks.values())
-                if attackDamage > best:
-                    best = attackDamage
+                if attackDamage < worst:
+                    worst = attackDamage
                     index = self._pokemonList.index(pokemon)
 
         if self._pokemon == self._pokemonList[index]:
@@ -148,29 +184,25 @@ class Trainer:
             if self._pokemon._hp- predicted_damage <= 0:
                 if not self.heal(True):
                     if not self.heal(False, True):
-                        if len(available_pokemon) == 3:
+                        if len(available_pokemon) <= 3:
                             self.switch_to_valid_pokemon()
                             return
+            # attack if not deadly
+            valid = self._pokemon.attack(attack_index, self._other)
+            while not valid:
+                largest.append(self.next_largest(strongest_attack))
+                attack_index = list(self._pokemon._attacks.values()).index(largest[-1]) + 1
                 valid = self._pokemon.attack(attack_index, self._other)
-                while not valid:
-                    largest.append(self.next_largest(strongest_attack))
-                    attack_index = list(self._pokemon._attacks.values()).index(largest[-1]) + 1
-                    valid = self._pokemon.attack(attack_index, self._other)
-                return
-            else:
-                valid = self._pokemon.attack(attack_index, self._other)
-                while not valid:
-                    largest.append(self.next_largest(strongest_attack))
-                    attack_index = list(self._pokemon._attacks.values()).index(largest[-1]) + 1
-                    valid = self._pokemon.attack(attack_index, self._other)
-                return
+            return
 
-        # Consider switching Pokémon or healing.
+        # Consider switching Pokémon or healing if damage can kill.
         if self._pokemon._hp - 1.5*predicted_damage <= 0:
-            if len(available_pokemon) > 1 and self._pokemon._hp <= 15:
+            # if available pokemon is 2 or 3 and hp is low
+            if 1 < len(available_pokemon) <= 3 and self._pokemon._fullhp/4 == self._hp:
                 self.switch_to_valid_pokemon()
                 return
             else:
+                # otherwise try to heal or attack
                 if not self.heal(True):
                     valid = self._pokemon.attack(attack_index, self._other)
                     while not valid:
@@ -179,14 +211,18 @@ class Trainer:
                         valid = self._pokemon.attack(attack_index, self._other)
                         return
 
+        # heal if hp is low
         elif self._pokemon._hp <= self._pokemon._fullhp / 3:
             if not self.heal():
+                # attack if cannot heal
                 valid = self._pokemon.attack(attack_index, self._other)
                 while not valid:
                     largest.append(self.next_largest(strongest_attack))
                     attack_index = list(self._pokemon._attacks.values()).index(largest[-1]) + 1
                     valid = self._pokemon.attack(attack_index, self._other)
                 return
+            
+        # otherwise attack
         else:
             valid = self._pokemon.attack(attack_index, self._other)
             while not valid:
@@ -194,3 +230,5 @@ class Trainer:
                 attack_index = list(self._pokemon._attacks.values()).index(largest[-1]) + 1
                 valid = self._pokemon.attack(attack_index, self._other)
             return
+
+        return
